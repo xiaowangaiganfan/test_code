39c53bfb9e7558e170a378e698c8c8e66844dfdf
DESKTOP-KMNUUF9\32283 <3228318421@qq.com>
2022-10-17 20:25:48 +0800

    test code

---
+++ jiegou/jiegou/jiegou1.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//
+//
+//struct student
+//{
+//	string name;
+//	int age;
+//	int socre;
+//
+//};
+//int main()
+//{
+//	struct student stuArray[3] =
+//	{
+//		{"张三",18,100},
+//		{"李四",28,99},
+//		{"王五",38,66}
+//	};
+//	stuArray[2].name = "赵六";
+//	stuArray[2].age = 80;
+//	stuArray[2].socre = 60;
+//
+//	for (int i = 0; i < 3;i++)
+//	{
+//		cout << "姓名：" << stuArray[i].name
+//			<< "年龄：" << stuArray[i].age
+//			<< "分数：" << stuArray[i].socre << endl;
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
